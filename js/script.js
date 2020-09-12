75e6cdea7ac13987719492e567ea4727701408be
aakhilv <65052071+aakhilv@users.noreply.github.com>
2020-09-12 12:17:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ function setPenColor(pen) {
 function setPixelColor(pixel) {
   pixel.style.backgroundColor = penColor;
 }
+
+function removegrid() {
+  document.getElementById("art").style.borderSpacing = "0px";
+}
+
+function enablegrid() {
+  document.getElementById("art").style.borderSpacing = "1px";
+}
+
+function removeborder() {
+  document.getElementById("art").style.borderStyle = "hidden";
+}
+
+function enableborder() {
+  document.getElementById("art").style.borderStyle = "solid";
+}
